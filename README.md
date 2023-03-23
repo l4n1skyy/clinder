b29a89b77ee4e7e80847754a2ff18b994a8afe92
Lanisha <lanishaanusri28@gmail.com>
2023-03-23 22:50:43 +0800

    added credential checker for google calendar api

---
+++ README.md
@@ -0,0 +1,4 @@
+# Clinder
+#### Video Demo:  <URL HERE>
+#### Description:
+TODO
---
+++ program.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    check_credentials()
+
+
+def check_credentials():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+        exit()
+
+    print("authentication successful")
+
+def create_event():
+    return
+def read_event():
+    return
+def update_event():
+    return
+def delete_event():
+    return
+
+if __name__ == '__main__':
+    main()
